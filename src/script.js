d0400602285b416bbc69667d9b46d32d467f1fd0
Samir Jadhav <samirzjadhav@gmail.com>
2023-09-28 10:14:34 +0530

    feat: added event on navbar

--- src/script.js
+++ src/script.js
@@ -2,8 +2,15 @@
 
 const closeBtn = document.getElementById("nav-close-btn");
 const mobileNav = document.querySelector(".mobile-navbar");
-console.log(mobileNav);
+const whyOtter = document.getElementById("why-otter");
+const navDropdown = document.getElementById("nav-dropdown");
+
+console.log(navDropdown);
 
 closeBtn.addEventListener("click", () => {
   mobileNav.classList.toggle("active");
+});
+
+whyOtter.addEventListener("click", () => {
+  navDropdown.classList.toggle("active");
 });
